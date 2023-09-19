9f653138d75941cd7174f7058813b0e002ea3aaf
Zoey Baker <zoeybaker625@gmail.com>
2023-09-19 15:16:02 -0500

    initial commit

---
+++ Learn Js 2.js
@@ -0,0 +1,63 @@
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+
+// Assigments 
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+// Math
+
+var sum = 10 + 10;
+console.log(sum)
+
+var difference = 45 - 33;
+console.log(difference)
+
+var product = 8 * 10;
+console.log(product)
+
+
+var quotient = 66 / 33;
+console.log(quotient)
+
+// Incrementing Numbers
+
+var myVar = 87;
+myVar++;
+console.log(myVar)
+
+var myVar = 11;
+myVar--;
+console.log(myVar)
+
+var ourDecimal = 5.7;
+var myDecimal = 0.009;
+console.log(myDecimal)
+
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+var quotient = 4.4 / 2.0;
+console.log(quotient)
+
+// Finding a Remainder
+ var remainder
+ remainder = 11 % 3;
+
+ // Compound Assignment with Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+console.log(a = 3, b = 17, c = 12);
+
+
